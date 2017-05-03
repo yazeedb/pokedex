aceb8c20cd4c5f2bec5847439d59405accda5d4d
yazeedb <ybzadough@gmail.com>
2017-05-03 06:54:19 -0400

    add cursor component

---
+++ src/app/shared/cursor/cursor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursor',
+  templateUrl: './cursor.component.html',
+  styleUrls: ['./cursor.component.scss']
+})
+export class CursorComponent {
+  ngOnInit() {
+    console.log('cursor component');
+  }
+}
