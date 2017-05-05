83c1eaf3397920d9fc594205e2e209484e68a531
yazeedb <ybzadough@gmail.com>
2017-05-05 09:58:08 -0400

    add keydown listener for cursor movement

--- src/app/shared/cursor/cursor.component.ts
+++ src/app/shared/cursor/cursor.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-cursor',
   templateUrl: './cursor.component.html',
   styleUrls: ['./cursor.component.scss']
 })
-export class CursorComponent {
+export class CursorComponent implements OnInit {
+  private keyMap = {
+    ArrowUp: true,
+    ArrowDown: true,
+    ArrowLeft: true,
+    ArrowRight: true
+  };
+
+  ngOnInit() {
+    Observable
+      .fromEvent(document, 'keydown')
+      .map((e: KeyboardEvent) => e.key)
+      .filter((key => this.keyMap[key]))
+      .subscribe(key => console.log(key));
+  }
 }
