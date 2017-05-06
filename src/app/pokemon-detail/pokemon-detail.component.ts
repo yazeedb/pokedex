53812fadbcf3a1dbe1c6990fa8e8bb34c17f496e
yazeedb <ybzadough@gmail.com>
2017-05-06 17:25:32 -0400

    use ActivatedRoute to get pokemon from api

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.scss']
 })
-export class PokemonDetailComponent {
+export class PokemonDetailComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {}
 
+  ngOnInit() {
+    this.activatedRoute
+      .params
+      .subscribe(params => {
+        this.api
+          .get('http://pokeapi.co/api/v2/pokemon/' + params.pokemon)
+          .subscribe(res => console.log(res));
+      });
+  }
 }
