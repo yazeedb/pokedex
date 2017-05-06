2e0cb16d94b5504ae4072860aeed4bdb3ff3a6af
yazeedb <ybzadough@gmail.com>
2017-05-06 16:59:39 -0400

    add pokemon-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CursorComponent } from './shared/cursor/cursor.component';
 import { HomeComponent } from './home/home.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 import { PokemonIdPipe } from './pokemon-list/pokemon-id.pipe';
 
@@ -20,7 +21,7 @@ import { PokemonListService } from './pokemon-list/pokemon-list.service';
     CursorComponent,
     HomeComponent,
     PokemonListComponent,
-
+    PokemonDetailComponent,
     PokemonIdPipe
   ],
   imports: [
---
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  templateUrl: './pokemon-detail.component.html',
+  styleUrls: ['./pokemon-detail.component.scss']
+})
+export class PokemonDetailComponent {
+
+}
