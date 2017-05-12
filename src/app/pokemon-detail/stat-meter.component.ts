a5711c952395906416ceb1a4920c6174f1175ac1
yazeedb <ybzadough@gmail.com>
2017-05-12 10:14:20 -0400

    add stat coloring logic and styles

--- src/app/pokemon-detail/stat-meter.component.ts
+++ src/app/pokemon-detail/stat-meter.component.ts
@@ -16,5 +16,20 @@ export class StatMeterComponent implements OnInit {
     const meter: HTMLElement = this.el.nativeElement.querySelector('span');
 
     meter.style.width = `${(this.stat * maxWidth) / 255}px`;
+    meter.classList.add(this.getStatClass(this.stat));
+  }
+
+  private getStatClass(stat: number) {
+    if (stat < 60) {
+      return 'poor';
+    } else if (stat < 80) {
+      return 'avg';
+    } else if (stat < 100) {
+      return 'above-avg';
+    } else if (stat < 150) {
+      return 'great';
+    } else {
+      return 'super';
+    }
   }
 }
