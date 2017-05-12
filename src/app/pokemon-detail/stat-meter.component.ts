f94812194c726a3e0c352f5fa54eaacf778c3b5a
yazeedb <ybzadough@gmail.com>
2017-05-12 08:58:30 -0400

    add meter width logic

--- src/app/pokemon-detail/stat-meter.component.ts
+++ src/app/pokemon-detail/stat-meter.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stat-meter',
   template: '<span></span>',
   styleUrls: ['./stat-meter.component.scss']
 })
-export class StatMeterComponent {
+export class StatMeterComponent implements OnInit {
+  constructor(private el: ElementRef) {}
 
+  @Input()
+  stat: number;
+
+  ngOnInit() {
+    const maxWidth = this.el.nativeElement.clientWidth;
+    const meter: HTMLElement = this.el.nativeElement.querySelector('span');
+
+    meter.style.width = `${(this.stat * maxWidth) / 255}px`;
+  }
 }
