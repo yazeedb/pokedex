8d4fe1c9c4357cf6d489008c892a6f171057316d
yazeedb <ybzadough@gmail.com>
2018-05-18 17:46:02 -0400

    serve assets and update eslint

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const { join } = require('path');
 const express = require('express');
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'assets')));
 
 app.get('*', (req, res) => {
-	res.sendFile(join(__dirname, 'src/index.html'));
+  res.sendFile(join(__dirname, 'src/index.html'));
 });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
