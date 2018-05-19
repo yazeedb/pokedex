a69545aedcde8feee313ba48f0d88ad6d3621710
yazeedb <ybzadough@gmail.com>
2018-05-19 18:30:13 -0400

    activate epics

--- src/store.js
+++ src/store.js
@@ -1,5 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import rootEpic from '/epics/root';
 import rootReducer from '/ducks/root';
 
-export default createStore(rootReducer, applyMiddleware(logger));
+export default createStore(
+  rootReducer,
+  applyMiddleware(
+    rootEpic,
+    logger
+  )
+);
