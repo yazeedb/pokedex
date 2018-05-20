a89cd9686278a37fe5d9905c3940c4c97de9f858
yazeedb <ybzadough@gmail.com>
2018-05-20 13:34:08 -0400

    grab proper response property in epic

--- src/features/PokemonPreviewList/epics.js
+++ src/features/PokemonPreviewList/epics.js
@@ -1,5 +1,5 @@
 import { of } from 'rxjs/observable/of';
-import { pluck, switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import { pokedex } from '/endpoints';
 import { fetchPokemon, setPokemon } from './duck';
@@ -7,9 +7,8 @@ import { fetchPokemon, setPokemon } from './duck';
 export const fetchPokemonEpic = (action$, store, { fetch$ }) => action$.pipe(
   ofType(fetchPokemon.type),
   switchMap(() => fetch$(pokedex).pipe(
-    pluck('response', 'pokemon_entries'),
-    switchMap(data => of(
-      setPokemon(data)
+    switchMap(({ response }) => of(
+      setPokemon(response)
     ))
   ))
 );
