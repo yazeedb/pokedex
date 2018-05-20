d1cda0e6519795dd8580cda72d990789a7a4d78b
yazeedb <ybzadough@gmail.com>
2018-05-20 13:31:12 -0400

    render pokemon list

--- src/features/PokemonPreviewList/component.js
+++ src/features/PokemonPreviewList/component.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { lifecycle } from 'recompose';
+import PokemonPreview from '/features/PokemonPreview';
 
 const Loading = () => <span data-test="loading">Loading...</span>;
 const Component = ({ fetching, list }) => (
   <div className="pokemon-preview-list">
-    { fetching ? <Loading /> : list }
+    {
+      fetching ? <Loading /> :
+        list.map(({ id, name, types }) => (
+          <PokemonPreview
+            name={ name }
+            types={ types }
+            spriteUrl={ `pokemon/icons/${id}.png` }
+            key={ id }
+          />
+        ))
+      }
   </div>
 );
 
--- test/features/PokemonPreviewList/component.spec.js
+++ test/features/PokemonPreviewList/component.spec.js
@@ -1,5 +1,7 @@
 import Component from '/features/PokemonPreviewList/component';
+import PokemonPreview from '/features/PokemonPreview';
 import { mountWrapper } from '/wrapper';
+import pokemonJson from '/api/data/pokemon.json';
 
 const getWrapper = mountWrapper(Component);
 
@@ -7,7 +9,10 @@ describe('PokemonPreviewList', () => {
   it('calls fetchPokemon on componentDidMount', () => {
     const fetchPokemon = jest.fn();
 
-    getWrapper({ fetchPokemon });
+    getWrapper({
+      fetchPokemon,
+      list: []
+    });
 
     expect(fetchPokemon.mock.calls.length).toBe(1);
     expect(fetchPokemon.mock.calls[0][0]).toBe(undefined);
@@ -16,10 +21,41 @@ describe('PokemonPreviewList', () => {
   it('shows loading indicator if fetching', () => {
     const loading = getWrapper({
       fetching: true,
-      fetchPokemon: () => {}
+      fetchPokemon: () => {},
+      list: []
     })
       .find('[data-test="loading"]');
 
     expect(loading.length).toBe(1);
+  });
+
+  it('renders a PokemonPreview for each list item', () => {
+    const list = pokemonJson.slice(0, 3);
+    const elementCount = getWrapper({
+      fetchPokemon: () => {},
+      fetching: false,
+      list
+    })
+      .find(PokemonPreview)
+      .length;
+
+    expect(elementCount).toBe(3);
+  });
+
+  it('passes the correct props to each PokemonPreview', () => {
+    const list = pokemonJson.slice(0, 1);
+    const { id, name, types } = list[0];
+    const componentProps = getWrapper({
+      fetchPokemon: () => {},
+      fetching: false,
+      list
+    })
+      .find(PokemonPreview)
+      .props();
+
+
+    expect(componentProps.name).toBe(name);
+    expect(componentProps.types).toBe(types);
+    expect(componentProps.spriteUrl).toBe(`pokemon/icons/${id}.png`);
   });
 });
