06cdf6bda06b17ae3280be416771125826d4156e
yazeedb <ybzadough@gmail.com>
2018-05-21 16:20:28 -0400

    finish build

--- build/getPokemonDetails.js
+++ build/getPokemonDetails.js
@@ -1,26 +1,44 @@
 const fs = require('fs');
 const fetch = require('isomorphic-fetch');
-const { Observable } = require('rxjs');
+
+const { from } = require('rxjs/observable/from');
+const { fromPromise } = require('rxjs/observable/fromPromise');
+const { forkJoin } = require('rxjs/observable/forkJoin');
+const { concatMap, map, toArray } = require('rxjs/operators');
+
+const pokemon = require('../src/api/data/pokemon.json');
 
 const getPokemonDetails = id => `https://pokeapi.co/api/v2/pokemon/${id}/`;
-const pokemon = require('../src/api/data/pokemon.json');
+const getPokemonSpecies = id => (
+  `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+);
 
 fs.writeFileSync('src/api/data/pokemonDetails.json');
 const out = fs.createWriteStream('src/api/data/pokemonDetails.json');
 
-Observable.from(pokemon)
-  .concatMap(({ id }) => Observable.fromPromise(
-    fetch(getPokemonDetails(id))
-      .then(res => res.json())
-      .then((json) => {
-        console.log('got', id);
+const fetchIt = url => fromPromise(
+  fetch(url).then(res => res.json())
+);
 
-        return json;
-      })
-  ))
-  .toArray()
-  .map(JSON.stringify)
+from(pokemon.slice(12, 13)).pipe(
+  map(({ id }) => ({
+    detailsUrl: getPokemonDetails(id),
+    speciesUrl: getPokemonSpecies(id)
+  })),
+  concatMap(({ detailsUrl, speciesUrl }) => (
+    forkJoin(fetchIt(detailsUrl), fetchIt(speciesUrl), (details, species) => {
+      console.log('returning', detailsUrl, 'and', speciesUrl);
+
+      return {
+        details,
+        species
+      };
+    })
+  )),
+  toArray(),
+  map(JSON.stringify)
+)
   .subscribe((result) => {
-    console.log('done, writing');
+    console.log('done');
     out.write(result);
   });
