f6af14038b9595c98ae9df69157b7e9ea406e90b
yazeedb <ybzadough@gmail.com>
2018-05-30 19:47:16 -0400

    set fill widths

--- src/components/StatMeter/index.js
+++ src/components/StatMeter/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
+import getRatingColor from './getRatingColor';
+import getRatingWidth from './getRatingWidth';
 import './StatMeter.scss';
 
-const StatMeter = ({ rating }) => (
-  <span className="stat-meter">
-    <span className="meter-fill" />
-  </span>
-);
+class StatMeter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ref = createRef();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { rating } = this.props;
+    const { color } = getRatingColor(rating);
+    const width = getRatingWidth(rating, this.ref.current);
+
+    /* eslint-disable react/no-did-mount-set-state */
+    this.setState({ color, width });
+    /* eslint-enable react/no-did-mount-set-state */
+  }
+
+  render() {
+    const { color, width } = this.state;
+
+    return (
+      <span className="stat-meter" ref={ this.ref }>
+        <span
+          className="meter-fill"
+          style={ {
+            backgroundColor: color,
+            width: `${width}px`
+          } }
+        />
+      </span>
+    );
+  }
+}
 
 StatMeter.propTypes = {
   rating: PropTypes.number
