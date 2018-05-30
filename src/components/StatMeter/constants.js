d14480743c1f3346818c391c6d2a67b1cbf64314
yazeedb <ybzadough@gmail.com>
2018-05-30 02:33:17 -0400

    add getRatingColor

--- src/components/StatMeter/constants.js
+++ src/components/StatMeter/constants.js
@@ -1,9 +1,34 @@
-export const statColorMap = {
-  poor: '#F8272E',
-  belowAverage: '#F95A30',
-  average: '#F98E34',
-  aboveAverage: '#37F993',
-  high: '#39FBFA'
-};
+export const ratingColors = [
+  {
+    min: 1,
+    max: 50,
+    color: '#F8272E',
+    name: 'poor'
+  },
+  {
+    min: 51,
+    max: 75,
+    color: '#F95A30',
+    name: 'belowAverage'
+  },
+  {
+    min: 76,
+    max: 100,
+    color: '#F98E34',
+    name: 'average'
+  },
+  {
+    min: 101,
+    max: 150,
+    color: '#37F993',
+    name: 'aboveAverage'
+  },
+  {
+    min: 151,
+    max: Infinity,
+    color: '#39FBFA',
+    name: 'great'
+  }
+];
 
-export default statColorMap;
+export default ratingColors;
