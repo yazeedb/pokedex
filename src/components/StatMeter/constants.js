06481d27568a760d0c9cedcc6034a4afe94c1ab9
yazeedb <ybzadough@gmail.com>
2018-05-30 01:38:38 -0400

    initial files

---
+++ src/components/StatMeter/constants.js
@@ -0,0 +1,9 @@
+export const statColorMap = {
+  poor: '#F8272E',
+  belowAverage: '#F95A30',
+  average: '#F98E34',
+  aboveAverage: '#37F993',
+  high: '#39FBFA'
+};
+
+export default statColorMap;
