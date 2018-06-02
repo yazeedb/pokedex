10c4853021870a71035e0f27bf9929727a938116
yazeedb <ybzadough@gmail.com>
2018-06-02 14:48:39 -0400

    initial server code

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const { join } = require('path');
 const express = require('express');
+const { __express } = require('ejs');
 const api = require('./src/api');
+const { root } = require('./webpack/helpers');
+
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const Routes = require('./src/Routes');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(join(__dirname, 'dist')));
-app.use(express.static(join(__dirname, 'assets')));
+app.engine('.ejs', __express);
+app.set('view engine', 'ejs');
+app.set('views', root('src'));
 
+app.use(express.static('dist'));
+app.use(express.static('assets'));
 app.use('/api', api(app, express));
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'src/index.html'));
+  const markup = ReactDOMServer.renderToStaticMarkup(<Routes />);
+
+  console.log(markup);
+  res.sendFile(root('src/index.html'));
 });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
