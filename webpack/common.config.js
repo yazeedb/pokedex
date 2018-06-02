7f80b7a88ce1c1a61085f03775955c7fc8c33a1c
yazeedb <ybzadough@gmail.com>
2018-06-02 14:10:36 -0400

    merge configs

---
+++ webpack/common.config.js
@@ -0,0 +1,37 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  output: { filename: 'bundle.js' },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '': root('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.png/,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new ExtractTextPlugin({ filename: '[name].css' })]
+};
