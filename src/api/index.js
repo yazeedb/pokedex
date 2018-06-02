585089d2f51b713fd660608fdf53b9e9a0fee900
yazeedb <ybzadough@gmail.com>
2018-06-02 15:01:13 -0400

    progress

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,17 @@
-const pokemon = require('./data/pokemon.json');
-const pokemonDetails = require('./data/pokemonDetails');
+// const pokemon = require('./data/pokemon.json');
+// const pokemonDetails = require('./data/pokemonDetails.json');
 
 module.exports = (app, express) => {
   const router = express.Router();
 
   router.get('/pokemon', (req, res) => {
-    res.json(pokemon);
+    res.json({ hello: 'world' });
   });
 
   router.get('/pokemon/:id', (req, res) => {
     const { id } = req.params;
 
-    res.json(pokemonDetails[id - 1]);
+    res.json({ hello: 'world' });
   });
 
   return router;
