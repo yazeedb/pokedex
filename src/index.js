ca0659272fbfdb6b93539d7709739bbfeaa96595
yazeedb <ybzadough@gmail.com>
2018-06-03 12:38:11 -0400

    conditionally render if isBrowser()

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
+import isBrowser from '/helpers/isBrowser';
 import store from './store';
 import Routes from './Routes';
 import './app.scss';
@@ -16,9 +17,12 @@ const App = (Router, props = {}) => () => (
 
 const BrowserApp = App(BrowserRouter);
 
-// render(
-//   <BrowserApp />,
-//   document.getElementById('root'),
-// );
+if (isBrowser()) {
+  render(
+    <BrowserApp />,
+    document.getElementById('root'),
+  );
+}
+
 
 export default App;
