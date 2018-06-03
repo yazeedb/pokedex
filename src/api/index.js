d26d036ee9843792ba8d8e6f076b3d4f941753a1
yazeedb <ybzadough@gmail.com>
2018-06-03 12:48:41 -0400

    never mind..

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,19 @@
-const pokemon = require('./data/pokemon.json');
-const pokemonDetails = require('./data/pokemonDetails.json');
+// const pokemon = require('./data/pokemon.json');
+// const pokemonDetails = require('./data/pokemonDetails.json');
 
 module.exports = (app, express) => {
   const router = express.Router();
 
   router.get('/pokemon', (req, res) => {
-    res.json(pokemon);
+    // res.json(pokemon);
+    res.json({ success: true });
   });
 
   router.get('/pokemon/:id', (req, res) => {
     const { id } = req.params;
 
-    res.json(pokemonDetails[id - 1]);
+    // res.json(pokemonDetails[id - 1]);
+    res.json({ success: true });
   });
 
   return router;
--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ if (isBrowser()) {
   const clientContent = document.getElementById('client-content');
   const serverContent = document.getElementById('server-content');
 
+  debugger;
+
   render(
     <BrowserApp />,
     clientContent,
