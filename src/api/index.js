705a6afc99027cab1143c716dbc45522e543a2e2
yazeedb <ybzadough@gmail.com>
2018-06-03 12:47:16 -0400

    add api logic back

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,17 @@
-// const pokemon = require('./data/pokemon.json');
-// const pokemonDetails = require('./data/pokemonDetails.json');
+const pokemon = require('./data/pokemon.json');
+const pokemonDetails = require('./data/pokemonDetails.json');
 
 module.exports = (app, express) => {
   const router = express.Router();
 
   router.get('/pokemon', (req, res) => {
-    res.json({ hello: 'world' });
+    res.json(pokemon);
   });
 
   router.get('/pokemon/:id', (req, res) => {
     const { id } = req.params;
 
-    res.json({ hello: 'world' });
+    res.json(pokemonDetails[id - 1]);
   });
 
   return router;
