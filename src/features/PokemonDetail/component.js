15de2563ab9c04b100dd75f933acb43f1f163bd0
yazeedb <ybzadough@gmail.com>
2018-06-06 19:15:58 -0400

    remove unused import

--- src/features/PokemonDetail/component.js
+++ src/features/PokemonDetail/component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { lifecycle } from 'recompose';
 import { head, pipe, prop } from 'ramda';
 import TypeBadge from '/components/TypeBadge';
