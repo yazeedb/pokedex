8ba698899711f90145974695568c1af76e57d9b0
yazeedb <ybzadough@gmail.com>
2019-04-01 20:20:24 -0400

    add PokemonDtail route

---
+++ src/screens/PokemonDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Component = () => {
+  return <div>Hello World</div>;
+};
+
+export const PokemonDetail = Component;
--- src/screens/Router.tsx
+++ src/screens/Router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { PokemonPreviewList } from './PokemonPreviewList';
 import { AppBar } from './shared/AppBar';
+import { PokemonDetail } from './PokemonDetail';
 
 export const Router = () => (
   <BrowserRouter>
     <AppBar />
     <Switch>
       <Route exact path="/" component={PokemonPreviewList} />
+      <Route exact path="/pokemon/:id" component={PokemonDetail} />
       <Route path="*" component={() => <div>404, not found!</div>} />
     </Switch>
   </BrowserRouter>
