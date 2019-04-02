391d3af85fe836e1dfa1ff2645a111337faa897e
yazeedb <ybzadough@gmail.com>
2019-04-01 20:46:15 -0400

    add 404 page

---
+++ src/screens/Page404.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as appTitle from '../store/AppTitle';
+import { bindActionCreators } from 'redux';
+import { Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+type Page404Props = {
+  actions: {
+    setTitle: typeof appTitle.setTitle;
+  };
+};
+
+export const Component: React.FunctionComponent<Page404Props> = ({
+  actions
+}) => {
+  React.useEffect(() => {
+    actions.setTitle('404 Page not found!');
+  });
+  return (
+    <div
+      style={{
+        margin: '0 auto',
+        textAlign: 'center'
+      }}
+    >
+      <Typography
+        variant="h4"
+        style={{
+          margin: '20px 0'
+        }}
+      >
+        We can't find that page!
+      </Typography>
+      <Link to="/">Go back home?</Link>
+      <img src="pokemon-icons/54.png" alt="Psyduck" />
+    </div>
+  );
+};
+
+const withRedux = connect(
+  null,
+  (dispatch) => ({
+    actions: {
+      setTitle: bindActionCreators(appTitle.setTitle, dispatch)
+    }
+  })
+);
+
+export const Page404 = withRedux(Component);
--- src/screens/Router.tsx
+++ src/screens/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { PokemonPreviewList } from './PokemonPreviewList';
 import { AppBar } from './shared/AppBar';
 import { PokemonDetail } from './PokemonDetail';
+import { Page404 } from './Page404';
 
 export const Router = () => (
   <BrowserRouter>
@@ -10,7 +11,7 @@ export const Router = () => (
     <Switch>
       <Route exact path="/" component={PokemonPreviewList} />
       <Route exact path="/pokemon/:id" component={PokemonDetail} />
-      <Route path="*" component={() => <div>404, not found!</div>} />
+      <Route path="*" component={Page404} />
     </Switch>
   </BrowserRouter>
 );
