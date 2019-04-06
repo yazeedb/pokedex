60e29d2acb32eaf40261534dfda0643653b7ec1f
yazeedb <ybzadough@gmail.com>
2019-04-06 05:35:00 -0400

    improve styling

--- src/screens/PokemonDetail.tsx
+++ src/screens/PokemonDetail.tsx
@@ -3,12 +3,12 @@ import * as pokemonDetail from '../store/PokemonDetail';
 import * as appTitle from '../store/AppTitle';
 import { PokemonDetailState } from '../store/PokemonDetail';
 import { connect } from 'react-redux';
-import { partition } from 'ramda';
 import { RootState } from '../store/rootReducer';
 import { bindActionCreators } from 'redux';
 import { FetchStatuses } from '../store/interfaces/FetchStatuses';
 import { Loading } from './shared/Loading';
 import { Chip, Typography, Grid } from '@material-ui/core';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
 import { typesToColors } from './shared/colors';
 
 type PokemonDetailProps = {
@@ -25,12 +25,14 @@ type PokemonDetailProps = {
 };
 
 const commonWrapperStyles = {
-  padding: '20px 0'
+  padding: '20px'
 };
 
 const oddWrapperBackgroundColor = '#F2F2F2';
 
 const renderWhenReady = (state: PokemonDetailState) => {
+  const isSmBreakpoint = useMediaQuery('(max-width:600px)');
+
   if (!state.pokemonDetail) {
     return null;
   }
@@ -38,34 +40,25 @@ const renderWhenReady = (state: PokemonDetailState) => {
   const { details, species } = state.pokemonDetail;
 
   return (
-    <div
-      style={{
-        textAlign: 'center'
-      }}
-    >
+    <div>
       <div
         style={{
           backgroundColor: oddWrapperBackgroundColor,
+          textAlign: 'center',
           ...commonWrapperStyles
         }}
       >
         <img src={`/pokemon-icons/${details.id}.png`} width="200px" />
       </div>
-      <div style={commonWrapperStyles}>
-        {details.types
-          .map((type) => type.type.name)
-          .map((typeName) => (
-            <Chip
-              label={typeName.toUpperCase()}
-              key={typeName}
-              style={{
-                backgroundColor: typesToColors[typeName],
-                color: 'white',
-                marginRight: '10px'
-              }}
-            />
-          ))}
-
+      <div
+        style={{
+          ...commonWrapperStyles,
+          maxWidth: '960px',
+          margin: '0 auto',
+          padding: '30px 10px',
+          textAlign: 'center'
+        }}
+      >
         <Typography
           variant="h3"
           style={{ textTransform: 'capitalize', marginTop: '15px' }}
@@ -76,57 +69,103 @@ const renderWhenReady = (state: PokemonDetailState) => {
           #{details.id} {species.genus}
         </Typography>
 
-        <Typography
-          variant="h6"
-          style={{ maxWidth: '900px', margin: '0 auto' }}
-        >
+        {details.types
+          .map((type) => type.type.name)
+          .map((typeName) => (
+            <Chip
+              label={typeName.toUpperCase()}
+              key={typeName}
+              style={{
+                backgroundColor: typesToColors[typeName],
+                color: 'white',
+                marginRight: '10px',
+                fontWeight: 'bold'
+              }}
+            />
+          ))}
+
+        <Typography variant="h6" style={{ marginTop: '15px' }}>
           {species.flavor_text}
         </Typography>
       </div>
-      <Grid
-        container
-        spacing={0}
-        xs={12}
+      <div
         style={{
-          backgroundColor: oddWrapperBackgroundColor,
-          ...commonWrapperStyles
+          ...commonWrapperStyles,
+          backgroundColor: oddWrapperBackgroundColor
         }}
       >
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6">Height : {details.height / 10} m</Typography>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6">
-            Weight : {details.weight / 10} kg
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
-            Generation : {species.generation.name}
-          </Typography>
-        </Grid>
+        <Grid
+          container
+          style={{
+            ...commonWrapperStyles,
+            maxWidth: '960px',
+            margin: '0 auto',
+            textAlign: isSmBreakpoint ? 'center' : 'initial'
+          }}
+        >
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6">
+              <b>Height</b> : {details.height / 10} m
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6">
+              <b>Weight</b> : {details.weight / 10} kg
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
+              <b>Generation</b> : {species.generation.name}
+            </Typography>
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
-            Egg Groups : {species.egg_groups.map((e) => e.name).join(', ')}
-          </Typography>
-        </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
+              <b>Egg Groups</b> :{' '}
+              {species.egg_groups.map((e) => e.name).join(', ')}
+            </Typography>
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6">
-            Capture Rate : {species.capture_rate}
-          </Typography>
-        </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6">
+              <b>Capture Rate</b> : {species.capture_rate}%
+            </Typography>
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
-            Abilities :{' '}
-            {details.abilities
-              .map((ability) => ability.ability.name)
-              .join(', ')}
-          </Typography>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6">
+              <b>Base Experience</b> : {details.base_experience}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6">
+              <b>Base Happiness</b> : {species.base_happiness}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6} style={{ textTransform: 'capitalize' }}>
+            <Typography variant="h6">
+              <b>Shape</b> : {species.shape.name}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6} style={{ textTransform: 'capitalize' }}>
+            <Typography variant="h6">
+              <b>Color</b> : {species.color.name}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
+              <b>Abilities</b> :{' '}
+              {details.abilities
+                .map((ability) => ability.ability.name)
+                .join(', ')}
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
