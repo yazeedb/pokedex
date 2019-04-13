4841ea2376e8245ce14cdda2bece7c1c1467ea7d
yazeedb <ybzadough@gmail.com>
2019-04-13 15:13:29 -0400

    virtualize pokemon list

--- src/screens/components/PokemonCard.tsx
+++ src/screens/components/PokemonCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { PokemonPreview } from '../../store/PokemonPreviewList';
 import {
-  Grid,
   Card,
   CardActionArea,
   Typography,
@@ -19,40 +18,39 @@ type PokemonCardProps = {
 export const PokemonCard: React.FunctionComponent<PokemonCardProps> = ({
   pokemon
 }) => (
-  <Grid item={true} xs={8} sm={4} md={3} lg={2}>
-    <Link to={`/pokemon/${pokemon.id}`} style={{ textDecoration: 'none' }}>
-      <Card
-        style={{
-          backgroundColor: colors.secondary
-        }}
-      >
-        <CardActionArea>
-          <Typography
-            variant="display1"
-            style={{
-              padding: '10px'
-            }}
-          >
-            {pokemon.id}
+  <Link to={`/pokemon/${pokemon.id}`} style={{ textDecoration: 'none' }}>
+    <Card
+      style={{
+        backgroundColor: colors.secondary,
+        height: '100%'
+      }}
+    >
+      <CardActionArea>
+        <Typography
+          variant="display1"
+          style={{
+            padding: '10px'
+          }}
+        >
+          {pokemon.id}
+        </Typography>
+        <CardMedia
+          image={`pokemon-icons/${pokemon.id}.png`}
+          title={pokemon.name}
+          component="img"
+          style={{
+            padding: '10px 20px'
+          }}
+        />
+        <CardContent>
+          <Typography variant="headline" noWrap>
+            {pokemon.name}
           </Typography>
-          <CardMedia
-            image={`pokemon-icons/${pokemon.id}.png`}
-            title={pokemon.name}
-            component="img"
-            style={{
-              padding: '10px 20px'
-            }}
-          />
-          <CardContent>
-            <Typography variant="headline" noWrap>
-              {pokemon.name}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <TypeBadge types={pokemon.types} style={{ marginRight: '5px' }} />
-          </CardActions>
-        </CardActionArea>
-      </Card>
-    </Link>
-  </Grid>
+        </CardContent>
+        <CardActions>
+          <TypeBadge types={pokemon.types} style={{ marginRight: '5px' }} />
+        </CardActions>
+      </CardActionArea>
+    </Card>
+  </Link>
 );
