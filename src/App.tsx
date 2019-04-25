362104023ac0814bc8cc041635f3f88063870b04
yazeedb <ybzadough@gmail.com>
2019-04-25 18:48:08 -0400

    add Elm TS declaration

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from './store/createStore';
 import { Router } from './screens/Router';
+import Elm from 'react-elm-components';
 
 export const App = () => {
   return (
